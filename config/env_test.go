0f4a0cb6014ab4eb91664404582a6adaabbb26ed
karabaralex <karabaralex@gmail.com>
2023-04-13 22:33:52 -0700

    fix test

--- config/env_test.go
+++ config/env_test.go
@@ -1,14 +1,26 @@
 package config
 
 import (
-    "testing"
 	"os"
+	"testing"
 )
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	os.Exit(code)
+}
+
+func setup() {
+	os.Setenv("RUTRACKER_LOGIN", "a")
+	os.Setenv("RUTRACKER_PASSWORD", "a")
+	os.Setenv("KVDB_TOKEN", "a")
+}
 
 func TestFolderNotSet(t *testing.T) {
 	os.Setenv("TORRENT_FOLDER", "")
 	os.Setenv("TELEGRAM_TOKEN", "")
-	_,error:=Read()
+	_, error := Read()
 	if error == nil {
 		t.Fatalf("expected error")
 	}
@@ -17,46 +29,46 @@ func TestFolderNotSet(t *testing.T) {
 func TestFolderSet(t *testing.T) {
 	os.Setenv("TORRENT_FOLDER", "my_folder")
 	os.Setenv("TELEGRAM_TOKEN", "token")
-	config,error:=Read()
+	config, error := Read()
 	if error != nil {
 		t.Fatalf("not expected error")
 	}
-	if config.TorrentFileFolder!="my_folder" {
+	if config.TorrentFileFolder != "my_folder" {
 		t.Fatalf("not expected %v", config.TorrentFileFolder)
 	}
-	if config.TelegramBotToken!="token" {
+	if config.TelegramBotToken != "token" {
 		t.Fatalf("not expected %v", config.TelegramBotToken)
 	}
 }
 
 func TestCreateFilePath(t *testing.T) {
-	actual:=CreateFilePath("path","file")
-	expected:="path/file"
+	actual := CreateFilePath("path", "file")
+	expected := "path/file"
 	if expected != actual {
-		t.Fatalf("expected %v, got %v", expected,actual)
+		t.Fatalf("expected %v, got %v", expected, actual)
 	}
 }
 
 func TestCreateFilePath2(t *testing.T) {
-	actual:=CreateFilePath("path/","file")
-	expected:="path/file"
+	actual := CreateFilePath("path/", "file")
+	expected := "path/file"
 	if expected != actual {
-		t.Fatalf("expected %v, got %v", expected,actual)
+		t.Fatalf("expected %v, got %v", expected, actual)
 	}
 }
 
 func TestCreateFilePath3(t *testing.T) {
-	actual:=CreateFilePath("/a/b/path/","file")
-	expected:="/a/b/path/file"
+	actual := CreateFilePath("/a/b/path/", "file")
+	expected := "/a/b/path/file"
 	if expected != actual {
-		t.Fatalf("expected %v, got %v", expected,actual)
+		t.Fatalf("expected %v, got %v", expected, actual)
 	}
 }
 
 func TestCreateFilePath4(t *testing.T) {
-	actual:=CreateFilePath("/a/b/path/","/file")
-	expected:="/a/b/path/file"
+	actual := CreateFilePath("/a/b/path/", "/file")
+	expected := "/a/b/path/file"
 	if expected != actual {
-		t.Fatalf("expected %v, got %v", expected,actual)
+		t.Fatalf("expected %v, got %v", expected, actual)
 	}
 }
